2f69ea79181a9bc5b1aeda7e3e65fbf67ee27d7b
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-04-26 03:21:43 -0700

    first commit

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,11 +1,14 @@
 import Container from "../Container";
+import Logo from "./Logo";
 
 const Navbar = () => {
     return ( 
         <div className="fixed z-10 w-full bg-white shadow-sm">
             <div className="py-4 border-b-[1px]">
                 <Container>
-                    anything
+                    <div className="flex flex-row justify-between gap-3 item-center md:gap-0">
+                        <Logo />
+                    </div>
                 </Container>
             </div>
         </div>
