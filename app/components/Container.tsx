fed11e3537315a39d91df02ceb88b084b2e5d827
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-04-25 21:01:03 -0700

    first commit

---
+++ app/components/Container.tsx
@@ -0,0 +1,13 @@
+interface ContainerProps {
+    children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({children}) => {
+    return ( 
+        <div>
+            {children}
+        </div>
+     );
+}
+ 
+export default Container;
---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import Container from "../Container";
+
+const Navbar = () => {
+    return ( 
+        <div className="fixed z-10 w-full bg-white shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                <Container>
+                    anything
+                </Container>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
